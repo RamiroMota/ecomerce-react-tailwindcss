f7599ffdf4554736995a4cd86f0f8a563272342c
Ramiro Mota <34577952+RamiroMota@users.noreply.github.com>
2023-08-11 02:00:33 -0600

    se agrego el icono de HOME

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+import Sidebar from './components/shared/Sidebar'
+
+function App() {
+  return (
+    <div className=' bg-gray-400 w-full main-h-screen'>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App
