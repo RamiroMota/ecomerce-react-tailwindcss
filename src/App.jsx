94dcda824374cd55e060399e47ea74264474467c
Ramiro Mota <rockett117@gmail.com>
2023-08-12 13:38:48 -0600

    se agregaron nuevos complementos

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Sidebar from './components/shared/Sidebar'
 
 function App() {
   return (
-    <div className=' bg-gray-400 w-full main-h-screen'>
+    <div className=' bg-gray-700 w-full min-h-screen'>
       <Sidebar />
     </div>
   );
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -5,11 +5,15 @@ import { SlHome } from 'react-icons/sl';
 function Sidebar() {
   return (
     <div className='bg-gray-800 fixed left-0 top-0 w-28 h-full'>
-        <img className='items-center my-2 p-4' src={logo} alt="Logo" />
-
-        <ul className='pl-4'>
+          <ul className='pl-4'>
+            <li>
+            <img className='items-center my-2 p-4' src={logo} alt="Logo" />
+            </li>
             <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl'>
-                <a className=' text-white p-4 block bg-cyan-600 hover:bg-cyan-500 rounded-xl' href=""><SlHome className=' text-2xl' /></a>
+                <a className=' text-white p-4 flex justify-center bg-cyan-600 rounded-xl' href=""><SlHome className=' text-2xl' /></a>
+            </li>
+            <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group'>
+                <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl' href=""><SlHome className=' text-2xl' /></a>
             </li>
         </ul>
     </div>
