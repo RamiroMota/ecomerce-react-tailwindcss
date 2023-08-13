98d5c18063c6402bca848661c157140c6e581767
RamiroMota <rockett117@gmail.com>
2023-08-13 03:47:07 -0600

    se avanzo hasta el menu movil

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { SlMenu, SlUser, SlPlus, SlPieChart } from 'react-icons/sl';
 import './index.css'
 import Sidebar from './components/shared/Sidebar'
 
@@ -6,6 +7,23 @@ function App() {
   return (
     <div className=' bg-gray-700 w-full min-h-screen'>
       <Sidebar />
+
+      {/* Menu movil */}
+
+      <nav className=' bg-gray-800 lg:hidden fixed w-full bottom-0 left-0 text-3xl py-4 px-8 flex items-center justify-between rounded-t-xl rounded-tr-xl text-gray-400'>
+        <button>
+          <SlUser />
+        </button>
+        <button>
+          <SlPlus />
+        </button>
+        <button>
+          <SlPieChart />
+        </button>
+        <button>
+          <SlMenu className=' text-white'/>
+        </button>
+      </nav>
     </div>
   );
 }
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -4,33 +4,40 @@ import { SlHome, SlFire, SlPieChart, SlEnvolope, SlBell, SlSettings, SlLogout } 
 
 function Sidebar() {
   return (
-    <div className='bg-gray-800 fixed left-0 top-0 w-28 h-full'>
-          <ul className='pl-4'>
-            <li>
-            <img className='items-center my-2 p-4' src={logo} alt="Logo" />
-            </li>
-            <li className=' bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl'>
-                <a className=' bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlHome className=' text-2xl' /></a>
-            </li>
-            <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
-                <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlFire className=' text-2xl' /></a>
-            </li>
-            <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
-                <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlPieChart className=' text-2xl' /></a>
-            </li>
-            <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
-                <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlEnvolope className=' text-2xl' /></a>
-            </li>
-            <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
-                <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlBell className=' text-2xl' /></a>
-            </li>
-            <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
-                <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlSettings className=' text-2xl' /></a>
-            </li>
-            <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
-                <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlLogout className=' text-2xl' /></a>
-            </li>
-        </ul>
+    <div className='bg-gray-800 fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl -left-full'>
+        <div>
+            <ul className='pl-4'>
+                <li>
+                    <img className='items-center my-2 p-4' src={logo} alt="Logo" />
+                </li>
+                <li className=' bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl'>
+                    <a className=' bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlHome className=' text-2xl' /></a>
+                </li>
+                <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
+                    <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlFire className=' text-2xl' /></a>
+                </li>
+                <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
+                    <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlPieChart className=' text-2xl' /></a>
+                </li>
+                <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
+                    <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlEnvolope className=' text-2xl' /></a>
+                </li>
+                <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
+                    <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlBell className=' text-2xl' /></a>
+                </li>
+                <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
+                    <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlSettings className=' text-2xl' /></a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <ul className='pl-4'>
+                <li className=' hover:bg-gray-700 p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
+                    <a className=' group-hover:bg-cyan-600 group-hover:text-white p-4 flex justify-center text-white rounded-xl transition-colors' href=""><SlLogout className=' text-2xl' /></a>
+                </li>
+            </ul>
+            
+        </div>
     </div>
   )
 }
